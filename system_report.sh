bd14929a1e3a99abf8c4570e4452f6c8bfe39351
Wiktoria Marzec <wiktoria.m.marzec@gmail.com>
2024-01-11 15:01:47 +0100

    system_report 1

--- system_report.sh
+++ system_report.sh
@@ -1,18 +1,18 @@
-get_cpu_info() {
+get_cpu_usage() {
     echo "CPU Usage:"
     top -bn1 | grep '%Cpu' | awk '{print "  CPU: " $2 "% user, " $4 "% system"}'
 
 }
 
-get_ram_info() {
+get_ram_usage() {
     echo "RAM Usage:"
     free -h | awk '/^Mem:/ {print "  Total: " $2, ", Used: " $3, ", Free: " $4}'
 }
 
 get_report() {
     echo "System Report:"
-    get_cpu_info
-    get_ram_info
+    get_cpu_usage
+    get_ram_usage
 }
 
 get_report
